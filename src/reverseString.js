e410be4375f0547052e753113106ab8d10d98ad7
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2026-04-06 16:32:20 +0530

    Improve formatting

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,9 +1,9 @@
 function reverseString(string) {
   const arr = Array.from(string);
-  
+
   let lo = 0;
   let hi = arr.length - 1;
-  
+
   while (lo < hi) {
     const temp = arr[lo];
     arr[lo] = arr[hi];
